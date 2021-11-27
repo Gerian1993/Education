29b8281e6146931f0fa63db7bcee2ef905cc1647
Ivan <i.generalov.93@mail.com>
2021-11-27 14:27:26 +0500

    Start creating movie app

--- Js/script.js
+++ Js/script.js
@@ -1,103 +1,20 @@
-"use strict";
+'use strict';
 
-/*let number = 5;
-const leftBorderWidth = 1;
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ``);
 
-number = 10;
-console.log(number);
-
-const obj = {
-    a: 50
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
 };
-console.log(obj);
+let q1 = prompt('Один из последних просмотренных фильмов?',``);
+let q2 = +prompt('На сколько оцените его?', ``);
+let q3 = prompt('Один из последних просмотренных фильмов?', ``);
+let q4 = +prompt('На сколько оцените его?',``);
 
-console.log(name);
-var name = `Ivan`; //Не использовать! Существует до того как она была объявлена.
-name= `Alex`;
+personalMovieDB.movies[q1] = q2;
+personalMovieDB.movies[q3] = q4;
 
-{
-    var result = 50;
-}
-
-console.log(result);*/
-
-//Data Types
-
-/*let number = 4.6;
-console.log(-4/0);
-console.log(`string`*9);
-
-const person = `Alex goes to the main Shop`;
-
-const bool = true;
-
-//console.log(somthing); //Null
-
-let und;
-console.log(und); //Undefiend
-
-const obj = {
-  name: "John",
-  age: 25,
-  isMarried: false,  
-};
-
-console.log(obj.name);
-//console.log(obj["name"]);
-
-
-let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-console.log(arr[1]);*/
-
-//Connection with client
-
-//alert (`Hello world!`);
-//const result = confirm(`Are you here?`);
-//console.log(result);
-
-//const answer = +prompt(`Вам есть 18?`,`18`);//Вся игформация приходящая от пользователя изначально будут строкой
-//console.log(typeof(answer));
-
-/*const answers = [];
-
-answers[0] = prompt(`Как ваше имя`);
-answers[1] = prompt(`Как ваше фамилия`);
-answers[2] = prompt(`Сколько вам лет`);
-
-console.log(typeof(answers));
-console.log(typeof(NaN));*/
-
-//Интерполяции
-
-/*const category = `toys`;
-
-console.log(`https://someurl.com/${category}/5`);
-
-
-const user = `Ivan`;
-
-console.log(`Hi, ${user}`);*/
-
-//Операторы
-
-console.log(`arr` + `- object`); //string конкотинация
-console.log(4 + `- object`); //string
-console.log(4 + +`5`); //number
-console.log(4 + +`object`); // Nan
-
-let incr = 10,
-    decr = 10;
-
-//incr++;
-//decr--;
-
-/*console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
-
-console.log(2 + 2 * 2 !== 8);
-
-const isChecked = false,
-      isClosed = true;
-console.log(isChecked || !isClosed);*/
+console.log(personalMovieDB)
