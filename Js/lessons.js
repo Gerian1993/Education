0c1bab076bc783f225896b55d0a45bd6c8b5c146
Ivan <i.generalov.93@mail.com>
2021-11-28 01:05:25 +0500

    training with fuction

--- Js/lessons.js
+++ Js/lessons.js
@@ -41,7 +41,7 @@ switch (num) {
 
 //Cycles
 
-let num = 50;
+/*let num = 50;
 
 while (num <= 55) {
     console.log(num);
@@ -58,4 +58,44 @@ for (let i = 0; i < 5; i++) {
         continue;
     }
     console.log(i);
+}*/
+
+//Functions
+
+let num = 20;
+
+function showFirstMesage (text) {
+    console.log(text);
+    num = 22;
+    console.log(text + num);
+    
 }
+
+showFirstMesage(`Hello world!`);
+
+function calc (a, b) {
+    return (a + b);
+    console.log(`bal`) //unreachable code
+}
+
+console.log(calc (1, 4));
+
+function ret () {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = console.log(ret());
+
+
+function bla () {} //function declaration, function work before called
+let bla1 = function () {}; // function expression, working after called
+const bla2 = () => {}; //arrow function, doesn't have context
+
+const calc1 = (a, b) => {
+    return(a+b);
+};
+
+console.log(calc1(1,4));
+
+const calc2 = (a, b) =>  (a+b);
