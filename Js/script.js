9233296e43590453ef4cfccfaa0f5704418ae6e3
Ivan <i.generalov.93@mail.com>
2021-11-27 18:38:16 +0500

    realize cycles and condition

--- Js/script.js
+++ Js/script.js
@@ -1,20 +1,40 @@
 'use strict';
 
+/* Задание на урок:
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ``);
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {},
+    movies: {},  //у объекта не может быть одинаковых ключей? 
     actors: {},
     genres: [],
     privat: false,
 };
-let q1 = prompt('Один из последних просмотренных фильмов?',``);
-let q2 = +prompt('На сколько оцените его?', ``);
-let q3 = prompt('Один из последних просмотренных фильмов?', ``);
-let q4 = +prompt('На сколько оцените его?',``);
 
-personalMovieDB.movies[q1] = q2;
-personalMovieDB.movies[q3] = q4;
+if (personalMovieDB.count < 10 && personalMovieDB.count != 0) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+
+for (let i = 0; i < 2; i++) {
+    let q1 = prompt('Один из последних просмотренных фильмов?',``);
+    while (q1 === `` || q1 === null || q1.length > 50) {
+           q1 = prompt('Не корректное название, пожалуйста повторите. Один из последних просмотренных фильмов?',``);   
+    }
+    let q2 = prompt('На сколько оцените его по дестибальной шкале?', ``);
+    while (q2 === `` || q2 === null || q2.length > 4) {
+           q2 = prompt('Не корректная оценка, пожалуйста повторите. На сколько оцените его?', ``);
+    }
+    personalMovieDB.movies[q1] = +q2;
+}
 
 console.log(personalMovieDB);
