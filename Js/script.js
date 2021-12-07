47ddb9d7f16e887cee4ab5820f797cfa7b25f00a
Ivan <i.generalov.93@mail.com>
2021-12-07 17:23:07 +0500

    Adiitional resolving move to another file

--- Js/script.js
+++ Js/script.js
@@ -37,25 +37,5 @@ for (let i = 0; i < 2; i++) {
     personalMovieDB.movies[q1] = +q2;
 }
 
-/*for (let i = 0; i < 2; i++) {
-    const q1 = prompt('Один из последних просмотренных фильмов?',``),
-          q2 = prompt('На сколько оцените его по дестибальной шкале?', ``);
-    if (q1 != `` && q2 != `` && q1 != null && q2 != null && q1.length < 50 && q2.length < 4) {
-        personalMovieDB.movies[q1] = +q2;
-        console.log(`ok`);
-    } else {
-        i--;
-        console.log(`mistake`);
-    }    
-}*/
-
-/*for (let i = 0; i < 2; i++) {
-    let q1 = prompt('Один из последних просмотренных фильмов?',``);
-    let q2 = prompt('На сколько оцените его по дестибальной шкале?', ``);
-    (q1 != `` || q2 != ``) ? personalMovieDB.movies[q1] = +q2 :
-    (q1 != null || q2 != null) ? personalMovieDB.movies[q1] = +q2 :
-    q1.length < 50 ? personalMovieDB.movies[q1] = +q2 :
-    i;
-}*/// Не знаю как через тернанрный оператор и цикл вополнить задачу.
 
 console.log(personalMovieDB);
