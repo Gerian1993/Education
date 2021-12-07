5504c8effe38c9d59e06eba8a95b1ed6bf8b7b50
Ivan <i.generalov.93@mail.com>
2021-12-07 18:29:59 +0500

    function practise

--- Js/script.js
+++ Js/script.js
@@ -4,7 +4,18 @@
 
 4) Потренироваться и переписать цикл еще двумя способами*/
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ``);
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ``);
+    while (numberOfFilms == `` || 
+    numberOfFilms == null || 
+    isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Не корректный ввод данных, пожвлуйста повторите. Сколько фильмов вы уже посмотрели?', ``);
+    }
+}
+
+start();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -14,28 +25,48 @@ let personalMovieDB = {
     privat: false,
 };
 
-if (personalMovieDB.count < 10 && personalMovieDB.count != 0) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let q1 = prompt('Один из последних просмотренных фильмов?',``);
+        while (q1 === `` || q1 === null || q1.length > 50) {
+               q1 = prompt('Не корректное название. Пожалуйста повторите, какой один из последних вами просмотренных фильмов?',``);   
+        }
+        let q2 = prompt('На сколько оцените его по дестибальной шкале?', ``);
+        while (q2 === `` || q2 === null || q2.length > 4) {
+               q2 = prompt('Не корректная оценка, пожалуйста повторите. На сколько оцените фильм?', ``);
+        }
+        personalMovieDB.movies[q1] = +q2;
+    }
 }
 
+rememberMyFilms();
 
-for (let i = 0; i < 2; i++) {
-    let q1 = prompt('Один из последних просмотренных фильмов?',``);
-    while (q1 === `` || q1 === null || q1.length > 50) {
-           q1 = prompt('Не корректное название. Пожалуйста повторите, какой один из последних вами просмотренных фильмов?',``);   
-    }
-    let q2 = prompt('На сколько оцените его по дестибальной шкале?', ``);
-    while (q2 === `` || q2 === null || q2.length > 4) {
-           q2 = prompt('Не корректная оценка, пожалуйста повторите. На сколько оцените фильм?', ``);
-    }
-    personalMovieDB.movies[q1] = +q2;
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10 && personalMovieDB.count != 0) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }    
 }
 
+detectPersonalLevel ();
 
-console.log(personalMovieDB);
+const showMyDB = () => {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+};
+showMyDB ();
+
+const writeYourGenres = function () {
+    for(let i = 0; i < 3; i++) {
+        const q1 = prompt(`Ваш любимый жанр по номером ${i+1} ?`);
+        personalMovieDB.genres.push(q1);
+    }
+};
+
+writeYourGenres();
